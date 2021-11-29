253461f72ac2ad22cd7c0fecf051b5e1b026d027
Eduardo Calzeta <ecalzeta@dahliacapital.com.br>
2021-11-29 17:54:24 -0300

    PosicaoCliente

---
+++ DUDS/DUDS/Models/PosicaoClientePassivoModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace DUDS.Models
+{
+    public class PosicaoClientePassivoModel
+    {
+        public Guid Id { get; set; }
+
+        public int CodInvestidorDistribuidor { get; set; }
+
+        public int CodFundo { get; set; }
+
+        public DateTime DataRef { get; set; }
+
+        public int CodAdministrador { get; set; }
+
+        public double QtdeCota { get; set; }
+
+        public double ValorCota { get; set; }
+
+        public double ValorBruto { get; set; }
+
+        public double Irrf { get; set; }
+
+        public double Iof { get; set; }
+
+        public double ValorLiquido  { get; set; }
+
+        public double PercRendimento { get; set; }
+
+        public double QtdeCotasBloqueado { get; set; }
+
+        public double ValorLiqBloqueado { get; set; }
+
+        public double ValorBrutoBloqueado { get; set; }
+
+        // Campos adicionais para facilitar acompanhar os respectivos codigos
+        public string NomeInvestidor { get; set; }
+
+        public string NomeFundo { get; set; }
+
+        public string NomeAdministrador { get; set; }
+    }
+}
---
+++ DUDS/DUDS/Service/Interface/IPosicaoClientePassivoService.cs
@@ -0,0 +1,20 @@
+﻿using DUDS.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DUDS.Service.Interface
+{
+    public interface IPosicaoClientePassivoService : IGenericOperationsService<PosicaoClientePassivoModel>
+    {
+        public Task<bool> AddBulkAsync(List<PosicaoClientePassivoModel> item);
+
+        public Task<bool> DeleteByDataRefAsync(DateTime? dataRef);
+
+        public Task<bool> DeleteAsync(Guid id);
+
+        // public Task<IEnumerable<PosicaoClientePassivoModel>> GetByDataRefAsync(DateTime dataRef);
+
+        public Task<IEnumerable<PosicaoClientePassivoModel>> GetByParametersAsync(DateTime? dataInicio, DateTime? dataFim, int? codDistribuidor, int? codGestor, int? codInvestidorDistribuidor);
+    }
+}
