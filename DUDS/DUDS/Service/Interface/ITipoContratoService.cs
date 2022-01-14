de6452182a527f557fa3b4e6801f64b293e4ea77
Eduardo Calzeta <ecalzeta@dahliacapital.com.br>
2022-01-14 10:17:54 -0300

    Update

--- DUDS/DUDS/Service/Interface/ITipoContratoService.cs
+++ DUDS/DUDS/Service/Interface/ITipoContratoService.cs
@@ -1,7 +1,5 @@
-﻿using DUDS.Models;
-using System;
+﻿using DUDS.Models.Tipos;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace DUDS.Service.Interface
