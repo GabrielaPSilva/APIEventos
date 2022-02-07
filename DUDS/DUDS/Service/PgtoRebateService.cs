5d54a4b1dee1d3088a1bab7abbe90051da2483c5
Gabriela Pinheiro <gpinheiro@dahliacapital.com.br>
2022-02-07 17:51:10 -0300

    Adicionado método que valida se a tabela erro pagamento bate com a tabela de pagamento rebate

--- DUDS/DUDS/Service/PgtoRebateService.cs
+++ DUDS/DUDS/Service/PgtoRebateService.cs
@@ -16,9 +16,42 @@ namespace DUDS.Service
             DefaultTypeMap.MatchNamesWithUnderscores = true;
         }
 
-        public Task<bool> ActivateAsync(int id)
+        public async Task<IEnumerable<PgtoRebateViewModel>> GetPgtoRebateByCompetencia(string competencia)
         {
-            throw new NotImplementedException();
+            using (var connection = await SqlHelpers.ConnectionFactory.ConexaoAsync())
+            {
+                const string query = IPgtoRebateService.QUERY_ARQUIVO_PGTO +
+                             @"
+                                 WHERE
+	                                tbl_pgto_rebate.Competencia = @Competencia";
+
+                return await connection.QueryAsync<PgtoRebateViewModel>(query, new { Competencia = competencia });
+            }
+        }
+
+        public async Task<IEnumerable<PgtoRebateViewModel>> GetValidaErrosPagamento(string competencia)
+        {
+            using (var connection = await SqlHelpers.ConnectionFactory.ConexaoAsync())
+            {
+                const string query = IPgtoRebateService.QUERY_ARQUIVO_PGTO +
+                             @"
+                                INNER JOIN 
+                                    tbl_erros_pagamento ON tbl_pgto_rebate.CodFundo = tbl_erros_pagamento.CodFundo AND
+													       tbl_pgto_rebate.ValorBruto = tbl_erros_pagamento.ValorBruto AND
+													       CpfCnpjFavorecido = tbl_erros_pagamento.CpfCnpjFavorecido
+				                WHERE 
+					                tbl_pgto_rebate.Competencia = @competencia";
+
+                return await connection.QueryAsync<PgtoRebateViewModel>(query, new { Competencia = competencia });
+            }
+        }
+
+        public async Task<IEnumerable<PgtoRebateModel>> GetPgtoRebateById(Guid Id)
+        {
+            using (var connection = await SqlHelpers.ConnectionFactory.ConexaoAsync())
+            {
+                return await connection.QueryAsync<PgtoRebateModel>(IPgtoRebateService.QUERY_BASE, new { Id = Id });
+            }
         }
 
         public async Task<bool> AddAsync(PgtoRebateModel item)
@@ -43,7 +76,7 @@ namespace DUDS.Service
             }
         }
 
-        public Task<bool> DeleteAsync(int id)
+        public Task<bool> UpdateAsync(PgtoRebateModel item)
         {
             throw new NotImplementedException();
         }
@@ -75,20 +108,17 @@ namespace DUDS.Service
             }
         }
 
+        public Task<bool> ActivateAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
         public Task<bool> DisableAsync(int id)
         {
             throw new NotImplementedException();
         }
 
-        public async Task<IEnumerable<PgtoRebateViewModel>> GetPgtoRebateByCompetencia(string competencia)
-        {
-            using (var connection = await SqlHelpers.ConnectionFactory.ConexaoAsync())
-            {
-                return await connection.QueryAsync<PgtoRebateViewModel>(IPgtoRebateService.QUERY_ARQUIVO_PGTO, new { Competencia = competencia });
-            }
-        }
-
-        public Task<bool> UpdateAsync(PgtoRebateModel item)
+        public Task<bool> DeleteAsync(int id)
         {
             throw new NotImplementedException();
         }
